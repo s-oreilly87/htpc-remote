c15729bebb86afd4740d656ceba991eb6f3dc3c4
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-04-30 11:01:11 -0400

    renamed/moved components

--- src/components/RemotePanels/Roku/RokuRemotePanel.js
+++ src/components/RemotePanels/Roku/RokuRemotePanel.js
@@ -1,13 +1,12 @@
 import Constants from "@/utilities/constants.js";
-import RokuChannels from "./ButtonGroups/RokuChannels";
-import HDMIInputs from "./ButtonGroups/HDMIInputs";
-import MediaButtons from "../Shared/ButtonGroups/MediaButtons";
-import BackHomeOption from "./ButtonGroups/BackHomeOption";
+import ChannelButtons from "./ChannelButtons";
+import HDMIInputButtons from "./HDMIInputButtons";
+import MediaButtons from "../Shared//MediaButtons";
+import BackHomeOption from "./BackHomeOption";
 import BottomSection from "../Shared/BottomSection";
 import {useEffect} from "react";
 import {fetchRokuDeviceInfo} from "@/utilities/http.js";
 import Overlay from "@/components/UI/Overlay.js";
-import findRokuDevices from "@/utilities/roku-ssdp.js";
 
 
 const remote = Constants.REMOTE.ROKU
@@ -41,13 +40,16 @@ function RokuRemote({ rokuState, setRokuState }) {
         <>
             <Overlay show={!rokuState.powerOn} />
             <div id="roku-remote" className="absolute panel-height w-full p-3 flex flex-col justify-between">
-                <div className="flex flex-col gap-4 justify-between">
-                    <RokuChannels setPowerOn={setRokuPowerOn}/>
-                    <HDMIInputs setPowerOn={setRokuPowerOn}/>
-                    <MediaButtons remote={remote} />
-                    {/*<button className="btn" onClick={searchForRoku}>Search For Roku</button>*/}
-                    <BackHomeOption
-                    />
+                <div className="flex flex-col flex-grow pb-[10%] gap-4 justify-between">
+                    <div className="flex flex-col gap-3">
+                        <ChannelButtons setPowerOn={setRokuPowerOn}/>
+                        <HDMIInputButtons setPowerOn={setRokuPowerOn}/>
+                    </div>
+                    <div className="flex flex-col flex-grow gap-3 justify-center">
+                        <MediaButtons remote={remote} />
+                        {/*<button className="btn" onClick={searchForRoku}>Search For Roku</button>*/}
+                        <BackHomeOption />
+                    </div>
                 </div>
                 <div className="h-50 items-end">
                     <BottomSection remote={remote} rokuState={rokuState} setRokuPowerOn={setRokuPowerOn} />
