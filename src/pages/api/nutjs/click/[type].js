c4bd23cab83fbda62b0757a4f14b38f806c4247a
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-04-30 10:35:49 -0400

    updated nutJS click endpoint to take 'type' instead of 'button', and added a case for 'double'

---
+++ src/pages/api/nutjs/click/[type].js
@@ -0,0 +1,22 @@
+import {mouse} from "@nut-tree/nut-js";
+import {CLICK_TYPE} from "@/utilities/constants.js";
+
+export default async function handleClick(req, res) {
+    let {type} = req.query   //.toUpperCase() //probably unnecessary with constants refactor
+
+    switch(type) {
+        case CLICK_TYPE.LEFT:
+            await mouse.click(0)
+            break
+        case CLICK_TYPE.RIGHT:
+            await mouse.click(2)
+            break
+        case CLICK_TYPE.DOUBLE:
+            await mouse.doubleClick(0)
+            break
+        default:
+            res.status(400).send(`No click 'type' specified. Values: {${CLICK_TYPE.LEFT}, ${CLICK_TYPE.RIGHT}, ${CLICK_TYPE.DOUBLE}}`)
+    }
+
+    res.status(200).send(`${type} click`)
+}
