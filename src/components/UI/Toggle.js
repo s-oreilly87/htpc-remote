8cdb415337e076a016e5e1b89b9f280308d9629d
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-04-30 10:33:18 -0400

    created toggle component

---
+++ src/components/UI/Toggle.js
@@ -0,0 +1,29 @@
+import {Switch} from "@headlessui/react";
+
+
+const Toggle = ({ enabled, onToggle, label, labelPos, color, disabled }) => {
+
+    return (
+        <Switch.Group>
+            <div className={`flex ${labelPos === 'above' ? 'flex-col' : ''} gap-3 items-center justify-center`}>
+                <Switch.Label className={`text-${color}`}>{ label }</Switch.Label>
+                <Switch
+                    checked={ enabled }
+                    onChange={ onToggle }
+                    className={`${ enabled ? `bg-${color}` : 'bg-slate-800'} \
+                        relative inline-flex h-6 w-11 items-center rounded-full`}
+                    disabled={ disabled }
+                >
+                    <span className="sr-only">Air Mouse</span>
+                    <span
+                        className={`${ enabled ? 'translate-x-6 bg-white' : `translate-x-1 ${disabled ? 'bg-gray-600' : `bg-${color}`}`} \
+                        inline-block h-4 w-4 transform rounded-full transition`}
+                    />
+                </Switch>
+            </div>
+        </Switch.Group>
+
+    )
+}
+
+export default Toggle
