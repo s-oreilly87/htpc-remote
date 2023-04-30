f94b6a2a2742cb4243032f1996183f0d8a30ba95
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-04-30 10:41:52 -0400

    removed rewrite for eventghost url - it has an endpoint now

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
-import Constants, {
-  DENON_HTTP_COMMAND_URL,
-  DENON_HTTP_QUERY_URL,
-  DENON_IP,
-  EVENTGHOST_URL,
-  ROKU_URL
-} from "./src/utilities/constants.js"
+import {DENON_HTTP_COMMAND_URL, DENON_HTTP_QUERY_URL, DENON_IP, ROKU_URL} from "./src/utilities/constants.js"
 
 
 const nextConfig = {
@@ -24,10 +18,6 @@ const nextConfig = {
       {
         source: '/api/denon-http/command/:cmd',
         destination: `http://${DENON_IP}/${DENON_HTTP_COMMAND_URL}%3F:cmd`
-      },
-      {
-        source: '/api/eventghost/:path*',
-        destination: `${EVENTGHOST_URL}/:path*`
       },
     ]
   },
