a55001ee61c010726e2317bb7ea47686f77c20c4
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-06-11 12:26:34 -0400

    added QR code when clicking Navbar remote icon

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -2,46 +2,57 @@ import Image from 'next/image'
 import {REMOTE} from "@/utilities/constants.js";
 import {useState} from "react";
 import {buttonPress} from "@/utilities/utils.js";
+import QRCode from "@/components/UI/QRCode.js";
 
 function Navbar({ className, selectedRemote, onClickHandler }) {
 
     const [buttonPressTimer, setButtonPressTimer] = useState()
+
+    const [qrModalOpen, setQrModalOpen] = useState(false)
 
     const handleClick = (event) => {
         onClickHandler(event)
         buttonPress(event.currentTarget, buttonPressTimer, setButtonPressTimer)
     }
 
+    const handleIconClick = () => {
+        setQrModalOpen(true)
+    }
+
     return (
-        <nav className={className}>
-            <div className="max-w-7xl h-16 w-full mx-auto px-3 z-10 flex relative justify-end xs:justify-center">
-                <div className="py-1 absolute left-0 top-0 aspect-video">
-                    <Image src={'/icons/app-icon.png'} alt="Remote" width={60} height={50} />
-                </div>
-                <div className="flex w-5/6 max-w-[550px] min-w-[270px] justify-center">
-                    <div className="w-full flex items-end">
-                        <button onClick={handleClick}
-                                className={ (selectedRemote === REMOTE.DENON ?
-                                    "nav-tab-active btn-primary-denon" : "nav-tab-inactive") }
-                                value={REMOTE.DENON}>
-                            Denon
-                        </button>
-                        <button onClick={handleClick}
-                                className={ (selectedRemote === REMOTE.ROKU ?
-                                    "nav-tab-active btn-primary-roku" : "nav-tab-inactive") }
-                                value={REMOTE.ROKU}>
-                            Roku
-                        </button>
-                        <button onClick={handleClick}
-                                className={ (selectedRemote === REMOTE.PC ?
-                                    "nav-tab-active btn-primary-pc" : "nav-tab-inactive") }
-                                value={REMOTE.PC}>
-                            HTPC
-                        </button>
+        <>
+            <QRCode isOpen={qrModalOpen} setIsOpen={setQrModalOpen} />
+            <nav className={className}>
+                <div className="max-w-7xl h-16 w-full mx-auto px-3 z-10 flex relative justify-end xs:justify-center">
+                    <div className="py-1 absolute left-0 top-0 aspect-video">
+                        <Image src={'/icons/app-icon.png'} alt="Remote" width={60} height={50} onClick={handleIconClick}/>
+                    </div>
+                    <div className="flex w-5/6 max-w-[550px] min-w-[270px] justify-center">
+                        <div className="w-full flex items-end">
+                            <button onClick={handleClick}
+                                    className={ (selectedRemote === REMOTE.DENON ?
+                                        "nav-tab-active btn-primary-denon" : "nav-tab-inactive") }
+                                    value={REMOTE.DENON}>
+                                Denon
+                            </button>
+                            <button onClick={handleClick}
+                                    className={ (selectedRemote === REMOTE.ROKU ?
+                                        "nav-tab-active btn-primary-roku" : "nav-tab-inactive") }
+                                    value={REMOTE.ROKU}>
+                                Roku
+                            </button>
+                            <button onClick={handleClick}
+                                    className={ (selectedRemote === REMOTE.PC ?
+                                        "nav-tab-active btn-primary-pc" : "nav-tab-inactive") }
+                                    value={REMOTE.PC}>
+                                HTPC
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
+
     );
 }
 
