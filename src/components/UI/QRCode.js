7364c5dcab80f9123ee07b30d13dccb66afdcc0d
Sean O'Reilly <sean.oreilly87@gmail.com>
2023-06-22 17:41:10 -0400

    Added link to download rootCA.pem to QR Code modal

--- src/components/UI/QRCode.js
+++ src/components/UI/QRCode.js
@@ -39,11 +39,14 @@ const QRCode = ({isOpen, setIsOpen}) => {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className="w-full max-w-md relative transform overflow-visible rounded-2xl bg-violet-950 p-6 text-left align-middle shadow-xl transition-all -top-3">
+                                <Dialog.Panel className="w-full max-w-md relative transform overflow-visible rounded-2xl bg-slate-800 p-6 text-left align-middle shadow-xl transition-all -top-3">
                                     <div className="w-full flex flex-col items-center justify-center gap-4">
                                         <Image width="300" height="300" src="/Remote App QR Code.png" alt="QR Code" />
                                         <div className="w-75 text-white text-center">
                                             For best experience, open app in Chrome browser.
+                                        </div>
+                                        <div className="w-75 text-white text-center">
+                                            Download and install &nbsp;<a download className="text-teal-500" href="rootCA.pem">rootCA.pem</a>&nbsp; to your device to remove security warning
                                         </div>
                                     </div>
                                 </Dialog.Panel>
