d5897aa5765d388335b49a5bc31c08b8ed999eac
Sean O'Reilly <100959160+s-oreilly87@users.noreply.github.com>
2025-11-27 18:33:06 -0500

    Remove vendored React type changes

--- vendor/@types/react-dom/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Minimal stub of @types/react-dom.
-import type { ReactElement, ReactNode } from '../react';
-
-export function render(element: ReactElement, container: Element | DocumentFragment | null, callback?: () => void): void;
-export function hydrate(element: ReactElement, container: Element | DocumentFragment | null, callback?: () => void): void;
-export function createPortal(child: ReactNode, container: Element | DocumentFragment, key?: null | string): ReactElement;
-export function unmountComponentAtNode(container: Element | DocumentFragment): boolean;
-
-export const version: string;
