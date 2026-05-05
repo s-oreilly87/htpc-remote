fcbff491f4254545bab1ea2c82009af8a9247cda
Sean O'Reilly <sean.oreilly@steeltoad.io>
2026-05-05 18:11:23 -0400

    feat: add windows htpc eventghost files + cleanup

---
+++ windows/htpc-agent/EventGhost/htpc-remote/utilities/soundvolumeviewGUI/Set DENON DTS-X.bat
@@ -0,0 +1,3 @@
+SoundVolumeView.exe /SetSpatial "DENON" "DTS:X for home"
+@REM SoundVolumeView.exe /SetDefault "DENON" "1" /SetSpeakersConfig "DENON" 0x63f 0x63f 0x60f /SetSpatial "DENON" "DTS:X for home"
+exit
---
+++ windows/htpc-agent/EventGhost/htpc-remote/utilities/soundvolumeviewGUI/Set DENON Stereo - 24-192.bat
@@ -0,0 +1,3 @@
+SoundVolumeView.exe /SetSpatial "DENON" "" /SetSpeakersConfig "DENON" 0x3 0x3 0x3
+@REM SoundVolumeView.exe /SetDefaultFormat "DENON" 24 192000
+exit
